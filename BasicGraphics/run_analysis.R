f64916b18bba962700177990038ceab637a63e37
Antoine <mayerantoine@gmail.com>
2017-06-21 22:08:30 -0400

    update version with more analysis.to review

--- BasicGraphics/run_analysis.R
+++ BasicGraphics/run_analysis.R
@@ -7,6 +7,7 @@ rawdata <- read_tsv("data/ICPI_FactView_PSNU_IM_20170515_v1_1_Haiti.txt")
 #-- must be UTF8 or update 
 
 # filter only needed indicators and cols
+#short partner names
 
 rm_indicators<- c("LAB_PT_TBAFB","CARE_COMM","VMMC_CIRC","INVS_COMD","LAB_PTCQI_LAB","LAB_PT_TBCULTURE",
                   "LAB_PT_EID","LAB_PT_CD4","LAB_CAP","KP_MAT","SC_STOCK","BS_SCREEN","BS_COLL","LAB_ACC",
@@ -82,7 +83,7 @@ ggplot(hts_tst_by_q)+
 # Indicator By Partner for Q2
 q2_ind <- c("HTS_TST","TX_NEW","TX_CURR","HTS_TST_POS",
             "PMTCT_ART","PMTCT_STAT","PMTCT_EID_POS","PMTCT_EID",
-            "TB_STAT","TB_STAT_POS","TB_ART","TX_TB","OVC_SERV","PMTCT_STAT_POS")
+            "TB_STAT","TB_STAT_POS","TB_ART","TX_TB","OVC_SERV","PMTCT_STAT_POS","KP_PREV","PP_PREV")
 
 ind_sum <- c("HTS_TST","TX_NEW","HTS_TST_POS", "PMTCT_ART","PMTCT_STAT","PMTCT_EID_POS","PMTCT_EID",
              "TB_STAT","TB_STAT_POS","PMTCT_STAT_POS")
@@ -104,7 +105,8 @@ indicator_by_partner <- fy2017q2 %>%
                         spread(indicator,fy2017_performance)
                         
 indicator_by_partner
-#write_csv(indicator_by_partner,"indicator_by_partner.csv")       
+write_csv(indicator_by_partner,"indicator_by_partnerv2.csv")       
+
 
 
 # HTC by Sex
@@ -128,5 +130,13 @@ htc_by_sex <- fy2017q2 %>%
     select(sex,fy2015q3,fy2015q4,fy2016q1,fy2016q2,fy2016q3,fy2016q4,fy2017q1,fy2017q2)
 htc_by_sex
 
-write.csv(htc_by_sex,"htc_by_sex.csv")
+#write.csv(htc_by_sex,"htc_by_sex.csv")
+
+# SNU HTC and HTC Pos
+snu_htc <- fy2017q2  %>% filter(indicator == "HTS_TST" | indicator == "HTS_TST_POS" ) %>% 
+    filter(snu1 != "_Military Haiti") %>%
+    filter(indicatortype == "DSD" ) %>%
+    filter(disaggregate == "Total Numerator")
+    select(psnu,fy2017q1,fy2017q2) %>%
+    gather("quarter","NbTested",fy2017q1:fy2017q2)
 
