42f6eb411b049b05400dcd1c7edbfcf1f83b6628
Antoine <mayerantoine@gmail.com>
2017-08-02 14:35:33 -0400

    July dataset script check

---
+++ ucsf_dataset.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(lubridate)
+
+patient_demo <-  read_tsv("Dataset_7_17_2017/patient_demo.txt")
+patient_cd4 <-  read_tsv("Dataset_7_17_2017/patient_cd4.txt")
+patient_art_ctx_event <-  read_tsv("Dataset_7_17_2017/patient_art_ctx_event.txt")
+patient_ptme <-  read_tsv("Dataset_7_17_2017/patient_ptme.txt")
+patient_regime <-  read_tsv("Dataset_7_17_2017/patient_regime.txt")
+patient_who_stage <-  read_tsv("Dataset_7_17_2017/patient_who_Stage.txt")
+
+## NA Fields Patient demo -- OK
+colSums(is.na(patient_demo))
+
+## NA Fields patient_cd4
+colSums(is.na(patient_cd4))
+
+patient_cd4_u <- patient_cd4 %>% select(Id,Numero,DateTest,Valeur,Source,ID_Patient)
+
+## NA Fields patient_art_ctx_event
+colSums(is.na(patient_art_ctx_event))
+
+patient_art_ctx_event_u <- patient_art_ctx_event %>% select(Id,EventType,EventDescription,Numero,EventDate,
+                                                           ID_Patient,Source)
+
+## NA Fields patient_ptme
+colSums(is.na(patient_ptme))
+
+patient_ptme_u <- patient_ptme %>% select(Id,DateEnrollement,DPA,Prophylaxie,Accouchement,
+                                          DateAccouchement,Allaitement,Statut,Source,ID_Patient)
+
+## NA Fields patient_regime
+colSums(is.na(patient_regime))
+
+patient_regime_u <- patient_regime %>% select(Id,TypeRegime,NomMedicament,DateDebut,DateFin,Dosage,Source,
+                                              ID_Patient)
+
+## NA Fields patient_who_stage
+colSums(is.na(patient_who_stage))
+
+patient_who_stage_u <- patient_who_stage %>% select(Id,Numero,ValeurStade,StartStade,EndStade,Source,ID_Patient)
+
+
+write_tsv(patient_demo, path="Dataset_7_17_2017/ucsf/patient_demo.txt")
+write_tsv(patient_cd4_u, path="Dataset_7_17_2017/ucsf/patient_cd4.txt")
+write_tsv(patient_art_ctx_event_u, path="Dataset_7_17_2017/ucsf/patient_art_ctx_event.txt")
+write_tsv(patient_ptme_u, path="Dataset_7_17_2017/ucsf/patient_ptme.txt")
+write_tsv(patient_regime_u, path="Dataset_7_17_2017/ucsf/patient_regime.txt")
+write_tsv(patient_who_stage, path="Dataset_7_17_2017/ucsf/patient_who_stage.txt")
