994c4238c5339bbae4d96dadd26b61010169022e
Antoine <mayerantoine@gmail.com>
2017-08-17 13:39:56 -0400

    Load Review savlh extract data ucsf

--- ucsf_dataset.R
+++ ucsf_dataset.R
@@ -1,12 +1,17 @@
 library(tidyverse)
 library(lubridate)
 
-patient_demo <-  read_tsv("Dataset_7_17_2017/patient_demo.txt")
-patient_cd4 <-  read_tsv("Dataset_7_17_2017/patient_cd4.txt")
-patient_art_ctx_event <-  read_tsv("Dataset_7_17_2017/patient_art_ctx_event.txt")
-patient_ptme <-  read_tsv("Dataset_7_17_2017/patient_ptme.txt")
-patient_regime <-  read_tsv("Dataset_7_17_2017/patient_regime.txt")
-patient_who_stage <-  read_tsv("Dataset_7_17_2017/patient_who_Stage.txt")
+
+setwd("C:/Users/wsn8/Documents/PEPFARDataAnalysis")
+
+
+rm(list=ls())
+patient_demo <-  read_tsv("FileBox/patient_demo.txt")
+patient_cd4 <-  read_tsv("FileBox/patient_cd4.txt")
+patient_art_ctx_event <-  read_tsv("FileBox/patient_art_ctx_event.txt")
+patient_ptme <-  read_tsv("FileBox/patient_ptme.txt")
+patient_regime <-  read_tsv("FileBox/patient_regime.txt")
+patient_who_stage <-  read_tsv("FileBox/patient_who_Stage.txt")
 
 ## NA Fields Patient demo -- OK
 colSums(is.na(patient_demo))
@@ -17,7 +22,7 @@ colSums(is.na(patient_cd4))
 patient_cd4_u <- patient_cd4 %>% select(Id,Numero,DateTest,Valeur,Source,ID_Patient)
 
 ## NA Fields patient_art_ctx_event
-colSums(is.na(patient_art_ctx_event))
+colSums(is.na(patient_art_ctx_event_u))
 
 patient_art_ctx_event_u <- patient_art_ctx_event %>% select(Id,EventType,EventDescription,Numero,EventDate,
                                                            ID_Patient,Source)
@@ -40,9 +45,8 @@ colSums(is.na(patient_who_stage))
 patient_who_stage_u <- patient_who_stage %>% select(Id,Numero,ValeurStade,StartStade,EndStade,Source,ID_Patient)
 
 
-write_tsv(patient_demo, path="Dataset_7_17_2017/ucsf/patient_demo.txt")
-write_tsv(patient_cd4_u, path="Dataset_7_17_2017/ucsf/patient_cd4.txt")
-write_tsv(patient_art_ctx_event_u, path="Dataset_7_17_2017/ucsf/patient_art_ctx_event.txt")
-write_tsv(patient_ptme_u, path="Dataset_7_17_2017/ucsf/patient_ptme.txt")
-write_tsv(patient_regime_u, path="Dataset_7_17_2017/ucsf/patient_regime.txt")
-write_tsv(patient_who_stage, path="Dataset_7_17_2017/ucsf/patient_who_stage.txt")
+write_tsv(patient_demo, path="FileBox/ucsf/patient_demo.txt")
+write_tsv(patient_cd4, path="FileBox/ucsf/patient_cd4.txt")
+write_tsv(patient_art_ctx_event, path="FileBox/ucsf/patient_art_ctx_event.txt")
+write_tsv(patient_ptme, path="FileBox/ucsf/patient_ptme.txt")
+write_tsv(patient_who_stage, path="FileBox/ucsf/patient_who_stage.txt")
